4ae1b2d367be8d4f210ca1da4aa184af7902c4ce
Karl Pawlowicz <karlpawlowicz@gmail.com>
2022-10-30 19:00:00 -0400

    feat: add serverless deployment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
+import { Server } from 'http';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { eventContext } from 'aws-serverless-express/middleware';
+import { createServer, proxy } from 'aws-serverless-express';
+import { Context, Handler } from 'aws-lambda';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+let cachedServer: Server;
+const express = require('express');
+const binaryMimeTypes: string[] = [];
+
+process.on('unhandledRejection', (reason) => {
+  console.error(reason);
+});
+
+process.on('uncaughtException', (reason) => {
+  console.error(reason);
+});
+
+async function bootstrap(): Promise<Server> {
+  if (!cachedServer) {
+    try {
+      const expressApp = express();
+      const app = await NestFactory.create(
+        AppModule,
+        new ExpressAdapter(expressApp),
+      );
+
+      app.use(eventContext());
+      await app.init();
+
+      cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  return Promise.resolve(cachedServer);
 }
-bootstrap();
+
+export const handler: Handler = async (event: any, context: Context) => {
+  cachedServer = await bootstrap();
+
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
