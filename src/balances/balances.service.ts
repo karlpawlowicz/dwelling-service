5bd94ddbf7ef2d6ce4a434ab38939fb4f2bb6f6e
Karl Pawlowicz <karlpawlowicz@gmail.com>
2022-10-31 16:25:46 -0400

    refactor: simplify findone method

--- src/balances/balances.service.ts
+++ src/balances/balances.service.ts
@@ -3,20 +3,18 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class BalancesService {
   findOne(cardNumber: string) {
+    let balance = 0;
+
     if (cardNumber[15] === '2' || cardNumber[15] === '5') {
-      return {
-        currentBalance: {
-          usd: cardNumber
-            .split('')
-            .reduce(
-              (previousValue, currentValue) =>
-                previousValue + parseInt(currentValue),
-              0,
-            ),
-        },
-      };
-    } else {
-      return { currentBalance: { usd: 0 } };
+      balance = cardNumber
+        .split('')
+        .reduce(
+          (previousValue, currentValue) =>
+            previousValue + parseInt(currentValue),
+          0,
+        );
     }
+
+    return { currentBalance: { usd: balance } };
   }
 }
